1edf3ad8691e794e483e6212b14f7718a96ad0d1
Lybio Moraes Junior <lybiomoraesjr@gmail.com>
2024-11-03 19:35:59 -0300

    Dockerfile deploy

--- src/main/java/br/com/lybiomoraesjr/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/lybiomoraesjr/todolist/task/ITaskRepository.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
     List<TaskModel> findByIdUser(UUID idUser);
 }
